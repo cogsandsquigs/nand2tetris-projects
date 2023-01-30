75a476c5c83bee1634152b9c85e8923b8e5423d7
Ian Pratt <ianjdpratt@gmail.com>
2023-01-30 15:08:46 -0600

    Finished Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,41 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Essentially, we repeatedly add R0 to R2, R1 times. In order to track how many times 
+// we added, we store that value in `counter`
+
+// First, we initialize R2 to 0, and counter to R1.
+@R2
+M=0
+
+@R1
+D=M
+@counter
+M=D // We set the counter to R1 because comparison operations check against 0, not some arbitrary value.
+
+// Then, we start a loop that will run R1 times.
+(LOOP)
+	// We check if we've added R0 R1 times.
+	@counter
+	D = M
+	@END
+	D; JLE // counter <= 0? (Jump comparisons check against 0)
+
+	// We add R0 to R2.
+	@R0
+	D = M
+	@R2
+	M = M + D
+	
+	// We decrement the counter.
+	@counter
+	M = M - 1
+
+	@LOOP
+	D; JMP
+
+// Finally, we end the program.
+(END)
+	// Infinte loop
+	@END
+	0; JMP
