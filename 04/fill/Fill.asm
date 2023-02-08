a727710ca18b1b7953c9b2e76143ce1bdb887402
Ian Pratt <ianjdpratt@gmail.com>
2023-02-08 15:07:58 -0600

    TODO: finish Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,57 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// To write to write pixel values (ones or zeros, white or black) to the screen, you address memory locations
+// 0x4000 to 0x5FFF, which correspond to the pixels of the screen.
+// Reading any input from the keyboard is done by reading the memory location 0x6000.
+
+// First, we check if a key is pressed. If so, we write "black" (0) to the screen.
+// If not, we write "white" (0) to the screen.
+
+(START)
+	@24576 // 0x6000 keyboard input
+	D=M // D=keyboard input
+
+	// Set `color` to 0 if a key is pressed, and to 1 otherwise
+	@KEY_PRESSED
+		D;JNE // if D!=0 (key pressed), jump to @color
+
+		@color
+		D=0 // D=0
+
+		// Jump to the end of the if statement
+		@END_KEY_PRESSED
+		0;JMP
+		
+		(KEY_PRESSED)
+		D=1 // D!=0
+
+	(END_KEY_PRESSED)
+	
+	@color
+	M=D // color=1 if a key is pressed, 0 otherwise
+
+	@counter
+	M=16384 // counter=0x4000
+
+	(LOOP)
+		@color // Get the color
+		D=M // D=color
+
+		// Write `color` to the screen
+		@counter
+		M=D
+
+		// Increment counter
+		@counter
+		M=M+1
+
+		// Check if we reached the end of the screen
+		D=M-24575 // D=counter-0x5FFF
+		
+		@LOOP
+		D;JGE // if D>=0 (counter>=0x1FFF), jump to LOOP
+
+	// Jump back to the start
+	@START
+	0;JMP
