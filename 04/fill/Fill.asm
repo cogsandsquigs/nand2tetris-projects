660c28e117bbc760847657872cd569ca42b49418
Ian Pratt <ianjdpratt@gmail.com>
2023-02-10 09:25:58 -0600

    changes

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -15,7 +15,7 @@
 // 0x4000 to 0x5FFF, which correspond to the pixels of the screen.
 // Reading any input from the keyboard is done by reading the memory location 0x6000.
 
-// First, we check if a key is pressed. If so, we write "black" (0) to the screen.
+// First, we check if a key is pressed. If so, we write "black" (-1) to the screen.
 // If not, we write "white" (0) to the screen.
 
 (START)
@@ -26,7 +26,7 @@
 	@KEY_PRESSED
 		D;JNE // if D!=0 (key pressed), jump to @color
 
-		@color
+		@0
 		D=0 // D=0
 
 		// Jump to the end of the if statement
@@ -34,35 +34,38 @@
 		0;JMP
 		
 		(KEY_PRESSED)
-		D=1 // D!=0
+		D=-1 // D!=0
 
 	(END_KEY_PRESSED)
 
-	@color
-	M=D // color=1 if a key is pressed, 0 otherwise
+	@0
+	M=D // color=-1 if a key is pressed, 0 otherwise
 
 	@16384
 	D=A // D=0x4000
-	@color 
+	@0 
 	M=D // counter=0x4000
 
 	(LOOP)
-		@color // Get the color
+		@0 // Get the color
 		D=M // D=color
 
-		// Write `color` to the screen
-		@counter
-		M=D
+		@1
+		A=M // Set memory to location described by counter
+		M=D // Write `color` to the screen
 
 		// Increment counter
-		@counter
+		@1
 		M=M+1
 
 		// Check if we reached the end of the screen
-		D=M-24575 // D=counter-0x5FFF
+		@24576
+		D=A // D=0x6000
+		@1
+		D=M-D // D=counter-0x6000
 
 		@LOOP
-		D;JGE // if D>=0 (counter-0x5FFF>=0), jump to LOOP
+		D;JGT // if D > 0 (counter-0x6000>0), jump to LOOP
 
 	// Jump back to the start
 	@START
